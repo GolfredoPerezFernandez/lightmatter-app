ddeb6cd066ed610d9afe180363276fd7b5cfffff
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2024-01-21 17:51:06 -0400

    asd

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
-    const canvasRef = useSignal<HTMLCanvasElement>();
+    const canvasRef = useSignal<any>();
 
     useVisibleTask$(() => {
         if (!canvasRef.value) {
